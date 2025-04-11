228b6a5231680f8c10249d199098b4f2ac6645f2
captainkryga <captainkryga@gmail.com>
2025-04-11 11:42:42 +0200

    add scenario model from training room

---
+++ Assets/Project/Scripts/Model/ScenarioModel.cs
@@ -0,0 +1,10 @@
+using Project.Scripts.Model.ScriptableObjects;
+
+namespace Project.Scripts.Model
+{
+	// Data set for the scenario in the training room
+	public static class ScenarioModel
+	{
+		public static ScenarioScriptable ScenarioScriptable { get; set; }
+	}
+}
