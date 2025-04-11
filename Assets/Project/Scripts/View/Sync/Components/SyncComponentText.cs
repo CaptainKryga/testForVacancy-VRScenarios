618823f8c750b9c8db433b0e8410625318cd4c22
captainkryga <captainkryga@gmail.com>
2025-04-11 12:45:47 +0200

    add Require in code

--- Assets/Project/Scripts/View/Sync/Components/SyncComponentText.cs
+++ Assets/Project/Scripts/View/Sync/Components/SyncComponentText.cs
@@ -1,8 +1,11 @@
 using Project.Scripts.View.Sync.Abstract;
 using TMPro;
+using UnityEngine;
 
 namespace Project.Scripts.View.Sync.Components
 {
+	[RequireComponent(typeof(TMP_Text))]
+	// component TMP_Text
 	public class SyncComponentText : SyncComponentAbstract<TMP_Text>
 	{
 		public override void UpdateByComponentT(TMP_Text component)
