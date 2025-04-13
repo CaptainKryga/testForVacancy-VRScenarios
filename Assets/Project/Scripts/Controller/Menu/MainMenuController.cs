d332929c79a4404965c261727bad54e34605b4f2
CaptainKryga <krygadenis@gmail.com>
2025-04-13 20:31:11 +0200

    disable logic mainmenucontroller from update sync logic

--- Assets/Project/Scripts/Controller/Menu/MainMenuController.cs
+++ Assets/Project/Scripts/Controller/Menu/MainMenuController.cs
@@ -5,12 +5,11 @@ namespace Project.Scripts.Controller.Menu
 	// main controller from menu
 	public class MainMenuController : MonoBehaviour
 	{
-		[SerializeField] private ControllerBaseAbstract _syncController;
+		// [SerializeField] private ControllerBaseAbstract _syncController;
 		
 		private void Start()
 		{
-			// Init sync view logic
-			_syncController.Init();
+			// _syncController.Init();
 		}
 	}
 }
