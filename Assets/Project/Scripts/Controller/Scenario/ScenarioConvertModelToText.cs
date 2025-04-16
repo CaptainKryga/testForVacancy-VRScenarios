8c69638143e2d3bfccd49073c08c87d555cdc3c1
captainkryga <captainkryga@gmail.com>
2025-04-16 13:26:54 +0200

    disable change color logic from task

--- Assets/Project/Scripts/Controller/Scenario/ScenarioConvertModelToText.cs
+++ Assets/Project/Scripts/Controller/Scenario/ScenarioConvertModelToText.cs
@@ -5,8 +5,8 @@ namespace Project.Scripts.Controller.Scenario
 {
 	public class ScenarioConvertModelToText
 	{
-		private readonly StringChange _stringChange = new();
-		private readonly ScenarioGetColor _scenarioGetColor = new();
+		// private readonly StringChange _stringChange = new();
+		// private readonly ScenarioGetColor _scenarioGetColor = new();
 
 		public string GetScenarioText(Model.ScenarioComponents.Scenario scenario)
 		{
@@ -14,16 +14,25 @@ namespace Project.Scripts.Controller.Scenario
 			ScenarioGroup[] groupSteps = scenario.Groups;
 			for (int groupIndex = 0; groupIndex < groupSteps.Length; groupIndex++)
 			{
-				result += _stringChange.SetColorFromString($"{groupIndex}. {groupSteps[groupIndex].Title}[{groupSteps[groupIndex].Description}]",
-					_scenarioGetColor.GetColorByStatus(groupSteps[groupIndex].Status)) + "\n";
+				result += 
+					// _stringChange.SetColorFromString(
+						$"{groupIndex}. {groupSteps[groupIndex].Title}[{groupSteps[groupIndex].Description}]";
+					// ,
+					// _scenarioGetColor.GetColorByStatus(groupSteps[groupIndex].Status)) + "\n";
 				for (int stepIndex = 0; stepIndex < groupSteps[groupIndex].Steps.Length; stepIndex++)
 				{
-					result += _stringChange.SetColorFromString($"  {stepIndex}) {groupSteps[groupIndex].Steps[stepIndex].Title}[{groupSteps[groupIndex].Steps[stepIndex].Description}]",
-					_scenarioGetColor.GetColorByStatus(groupSteps[groupIndex].Steps[stepIndex].Status)) + "\n";
+					result += 
+						// _stringChange.SetColorFromString(
+							$"  {stepIndex}) {groupSteps[groupIndex].Steps[stepIndex].Title}[{groupSteps[groupIndex].Steps[stepIndex].Description}]";
+						// ,
+					// _scenarioGetColor.GetColorByStatus(groupSteps[groupIndex].Steps[stepIndex].Status)) + "\n";
 					for (int actionIndex = 0; actionIndex < groupSteps[groupIndex].Steps[stepIndex].Actions.Length; actionIndex++)
 					{
-						result += _stringChange.SetColorFromString($"\t{actionIndex}) {groupSteps[groupIndex].Steps[stepIndex].Actions[actionIndex].Description}",
-						_scenarioGetColor.GetColorByStatus(groupSteps[groupIndex].Steps[stepIndex].Actions[actionIndex].Status)) + "\n";
+						result += 
+							// _stringChange.SetColorFromString(
+								$"\t{actionIndex}) {groupSteps[groupIndex].Steps[stepIndex].Actions[actionIndex].Description}";
+							// ,
+						// _scenarioGetColor.GetColorByStatus(groupSteps[groupIndex].Steps[stepIndex].Actions[actionIndex].Status)) + "\n";
 					}
 				}
 			}
