b3f287d468fb13bf7b07f1cab3620c81d0310fde
captainkryga <captainkryga@gmail.com>
2025-04-17 11:12:43 +0200

    add sound view

---
+++ Assets/Project/Scripts/View/TrainingRoom/ScenarioSoundView.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+namespace Project.Scripts.View.TrainingRoom
+{
+	public class ScenarioSoundView : MonoBehaviour
+	{
+		[SerializeField] private AudioSource _audioSource;
+		[SerializeField] private AudioClip _acSuccess;
+		[SerializeField] private AudioClip _acFail;
+		public void PlaySuccess(bool isSuccess)
+		{
+			_audioSource.clip = isSuccess ? _acSuccess : _acFail;
+			_audioSource.Play();
+		}
+	}
+}
